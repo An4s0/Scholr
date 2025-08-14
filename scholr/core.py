508f450a509d6783a414b0ac9e42ecd92dc298a7
Anas Almutary <00063q@gmail.com>
2025-08-14 07:47:10 +0300

    add script to get all pubs

--- scholr/core.py
+++ scholr/core.py
@@ -6,8 +6,29 @@ GOOGLE_SCHOLAR_URL = "https://scholar.google.com"
 def get_scholar_profile(profile: str) -> dict:
     if "scholar.google." not in profile:
         profile = f"{GOOGLE_SCHOLAR_URL}/citations?user={profile}"
-    
-    html = html_get(profile)
-    profile_info = parse_scholar_profile(html)
+
+    profile_info = None
+    all_pubs = []
+    start = 0 
+
+    while True:
+        url = f"{profile}&cstart={start}&pagesize=100&hl=en&view_op=list_works"
+        html = html_get(url)
+        if not html or "Please show you're not a robot" in html:
+            print("Blocked by Google Scholar or empty page")
+            break
+
+        page_info = parse_scholar_profile(html)
+
+        if profile_info is None:
+            profile_info = {k: v for k, v in page_info.items() if k != "publications"}
+
+        pubs = page_info.get("publications", [])
+        if len(pubs) == 1:
+            break
+        all_pubs.extend(pubs)
+        start += 100
+
+    profile_info["publications"] = all_pubs
 
     return profile_info

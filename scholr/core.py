b5edf92ccb8bcacdbd3566d2378d6e01035683ef
Anas Almutary <00063q@gmail.com>
2025-08-17 02:29:01 +0300

    add comments

--- scholr/core.py
+++ scholr/core.py
@@ -1,18 +1,21 @@
 from .utils import html_get
 from .parser import parse_scholar_profile, parse_publication_details
 
-GOOGLE_SCHOLAR_URL = "https://scholar.google.com"
+GOOGLE_SCHOLAR_URL = "https://scholar.google.com/citations?user="
 
-def get_scholar_profile(profile: str) -> dict:
-    if "scholar.google." not in profile:
-        profile = f"{GOOGLE_SCHOLAR_URL}/citations?user={profile}"
+def get_scholar_profile(user_id: str) -> dict:
+    """
+    Fetch the Google Scholar profile information for a given user ID.
 
+    :param user_id: The user ID of the Google Scholar profile.
+    :return: A dictionary containing the user's profile information.
+    """
     profile_info = None
     all_pubs = []
     start = 0 
 
     while True:
-        url = f"{profile}&cstart={start}&pagesize=100&hl=en&view_op=list_works"
+        url = f"{GOOGLE_SCHOLAR_URL}{user_id}&cstart={start}&pagesize=100&hl=en&view_op=list_works"
         html = html_get(url)
         if not html or "Please show you're not a robot" in html:
             print("Blocked by Google Scholar or empty page")
@@ -33,13 +36,14 @@ def get_scholar_profile(profile: str) -> dict:
 
     return profile_info
 
-def get_publication_details(publication: dict) -> dict:
-    # publication : publication url
-    if "scholar.google." not in publication:
-        print("Invalid publication URL")
-        return {}
+def get_publication_details(publication_url: str) -> dict:
+    """
+    Fetch the details of a specific publication from Google Scholar.
 
-    html = html_get(publication)
+    :param publication_url: The URL of the publication on Google Scholar.
+    :return: A dictionary containing the publication details.
+    """
+    html = html_get(publication_url)
     if not html or "Please show you're not a robot" in html:
         print("Blocked by Google Scholar or empty page")
         return {}
